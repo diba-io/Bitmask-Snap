87be72e78dbe0abb5e1b26aee126c97b243f07b8
Hunter Trujillo <cryptoquick@pm.me>
2024-03-26 15:55:56 -0600

    Formatting. Remove linters.

--- packages/snap/src/state.ts
+++ packages/snap/src/state.ts
@@ -19,33 +19,33 @@ export interface Booking extends UserAccount {
 
 export async function getUserAccount() {
   return {
-      userName: "Wesley",
-      lastName: "Parr",
-      frequentFlyerNumber: "JHF7889JVB"
-  }
+    userName: 'Wesley',
+    lastName: 'Parr',
+    frequentFlyerNumber: 'JHF7889JVB',
+  };
 }
 
 export async function getBooking() {
   const snapState = await snap.request({
-      method: 'snap_manageState',
-      params: {
-          operation: ManageStateOperation.GetState,
-          encrypted: false,
-      },
+    method: 'snap_manageState',
+    params: {
+      operation: ManageStateOperation.GetState,
+      encrypted: false,
+    },
   });
   return (snapState?.booking || {}) as Booking;
 }
 
 export async function initBooking() {
   await snap.request({
-      method: 'snap_manageState',
-      params: {
-          operation: ManageStateOperation.UpdateState,
-          newState: {
-              booking: {}
-          },
-          encrypted: false,
+    method: 'snap_manageState',
+    params: {
+      operation: ManageStateOperation.UpdateState,
+      newState: {
+        booking: {},
       },
+      encrypted: false,
+    },
   });
 }
 
@@ -79,57 +79,60 @@ export async function getInterfaceState(id: string) {
 export async function validateForm(id: string, formName: string) {
   const errors: string[] = [];
   switch (formName) {
-      case "formName": {
-          const interfaceState = await getInterfaceState(id);
-          const formValue = interfaceState[formName] as Booking;
-          if (!formValue.entropy || formValue.entropy.length == 0) {
-            errors.push("Entropy value cannot be empty")
-          }
-          if (!formValue.userName || formValue.userName.length == 0) {
-              errors.push("User name cannot be empty")
-          }
-          await updateBooking({
-              entropy: formValue.entropy || "",
-              userName: formValue.userName || ""
-          });
-          break;
+    case 'formName': {
+      const interfaceState = await getInterfaceState(id);
+      const formValue = interfaceState[formName] as Booking;
+      if (!formValue.entropy || formValue.entropy.length == 0) {
+        errors.push('Entropy value cannot be empty');
       }
+      if (!formValue.userName || formValue.userName.length == 0) {
+        errors.push('User name cannot be empty');
+      }
+      await updateBooking({
+        entropy: formValue.entropy || '',
+        userName: formValue.userName || '',
+      });
+      break;
+    }
 
-      case "formFlightDate": {
-          const interfaceState = await getInterfaceState(id);
-          const formValue = interfaceState[formName] as Booking;
-          if (!formValue.startDate || formValue.startDate.length == 0) {
-              errors.push("Start date cannot be empty")
-          }
-          if (!formValue.returnDate || formValue.returnDate.length == 0) {
-              errors.push("Return date cannot be empty")
-          }
-          if ((formValue.flexDays as any) == 'undefined') {
-              errors.push("Flex days should be 0 or more")
-          }
-          await updateBooking({
-              startDate: formValue.startDate || "",
-              returnDate: formValue.returnDate || "",
-              flexDays: (formValue.flexDays as any) == 'undefined' ? 0 : formValue.flexDays as number
-          });
-          break;
+    case 'formFlightDate': {
+      const interfaceState = await getInterfaceState(id);
+      const formValue = interfaceState[formName] as Booking;
+      if (!formValue.startDate || formValue.startDate.length == 0) {
+        errors.push('Start date cannot be empty');
       }
+      if (!formValue.returnDate || formValue.returnDate.length == 0) {
+        errors.push('Return date cannot be empty');
+      }
+      if ((formValue.flexDays as any) == 'undefined') {
+        errors.push('Flex days should be 0 or more');
+      }
+      await updateBooking({
+        startDate: formValue.startDate || '',
+        returnDate: formValue.returnDate || '',
+        flexDays:
+          (formValue.flexDays as any) == 'undefined'
+            ? 0
+            : (formValue.flexDays as number),
+      });
+      break;
+    }
 
-      case "formAirport": {
-          const interfaceState = await getInterfaceState(id);
-          const formValue = interfaceState[formName] as Booking;
-          if (!formValue.fromAirportCode || formValue.fromAirportCode.length == 0) {
-              errors.push("Starting Airport code cannot be empty")
-          }
-          if (!formValue.toAirportCode || formValue.toAirportCode.length == 0) {
-              errors.push("Destination Airport code cannot be empty")
-          }
-          await updateBooking({
-              fromAirportCode: formValue.fromAirportCode || "",
-              toAirportCode: formValue.toAirportCode || ""
-          });
-          break;
+    case 'formAirport': {
+      const interfaceState = await getInterfaceState(id);
+      const formValue = interfaceState[formName] as Booking;
+      if (!formValue.fromAirportCode || formValue.fromAirportCode.length == 0) {
+        errors.push('Starting Airport code cannot be empty');
       }
+      if (!formValue.toAirportCode || formValue.toAirportCode.length == 0) {
+        errors.push('Destination Airport code cannot be empty');
+      }
+      await updateBooking({
+        fromAirportCode: formValue.fromAirportCode || '',
+        toAirportCode: formValue.toAirportCode || '',
+      });
+      break;
+    }
   }
   return errors;
 }
